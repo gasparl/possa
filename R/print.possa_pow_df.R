b38fcdc1e5f7d453a02c8ac41dbeb886eb106e90
gasparl <lkcsgaspar@gmail.com>
2022-05-01 12:48:29 +0200

    files for overhauled printing, S3method

---
+++ R/print.possa_pow_df.R
@@ -0,0 +1,280 @@
+#'@title Print pow results
+#'
+#'@description Prints, in a readable manner, the main information from the list
+#'  created by the \code{\link[POSSA:pow]{POSSA::pow}} function, calling
+#'  \code{\link{print.possa_pow_df}} for each of the POSSA power information
+#'  data frames in the list. This is an extension (method) of the base R
+#'  \code{\link[print]} function, so it can be called simply as \code{print()}.
+#'@param list_pow The \code{\link{list}} returned by the
+#'  \code{\link[POSSA:pow]{POSSA::pow}} function.
+#'@param round_to Number of fractional digits to round to, for the displayed
+#'  numbers. The default is the value passed from the
+#'  \code{\link[POSSA:pow]{POSSA::pow}} function (stored in the returned list).
+#'
+#'@seealso \code{\link{pow}}, \code{\link{print.possa_pow_df}}
+#'
+#' @export
+print.possa_pow_list = function(list_pow,
+                                round_to = NA,
+                                ...) {
+    if (is.na(round_to)) {
+        round_to = list_pow$arguments$round_to
+    }
+    cat('\033[0;34m# POSSA pow() results #\033[0m', fill = TRUE)
+    for (possa_pow_list_elem in list_pow) {
+        if ('possa_pow_df' %in% class(possa_pow_list_elem)) {
+            print.possa_pow_df(possa_pow_list_elem,
+                               round_to = round_to,
+                               possa_title = FALSE,
+                               ...)
+        }
+    }
+}
+
+#'@title Print pow results data frame
+#'
+#'@description Prints, in a readable manner, the main information from any of
+#'  the data frames containing power information from the list created by the
+#'  \code{\link[POSSA:pow]{POSSA::pow}} function. This is an extension (method)
+#'  of the base R \code{\link[print]} function, so it can be called simply as
+#'  \code{print()}.
+#'@param list_pow The \code{\link{list}} returned by the
+#'  \code{\link[POSSA:pow]{POSSA::pow}} function.
+#'@param round_to Number of fractional digits to round to, for the displayed
+#'  numbers (\code{5} by default).
+#'
+#'@seealso \code{\link{pow}}, \code{\link{print.possa_pow_list}}
+#'
+#' @export
+print.possa_pow_df = function(df_pow,
+                              round_to = 5,
+                              possa_title = TRUE,
+                              ...) {
+    cat(
+        format.possa_pow_df(
+            df_pow = df_pow,
+            round_to = round_to,
+            possa_title = possa_title,
+            ...
+        )
+    )
+}
+
+format.possa_pow_df = function(df_pow,
+                               round_to = 5,
+                               possa_title = TRUE,
+                               ...) {
+    if (possa_title) {
+        cat('\033[0;34m# POSSA pow() results (df) #\033[0m', fill = TRUE)
+    }
+    df_nrow = nrow(df_pow)
+    mlook = df_nrow - 1
+    looks = 1:mlook
+    if (!(all(names(df_pow)[1:3] == c('look', 'n', 'n_rate')) &&
+          df_pow$look[df_nrow] == 'totals' &&
+          all(df_pow$look[1:(df_nrow - 1)] == 1:(df_nrow - 1)))) {
+        warning(
+            'Caution!: This data.frame does not seem to have the proper structure',
+            ' as created via POSSA::sim().'
+        )
+    }
+    df_p_names = c()
+    df_p_nams_all = c()
+    futilities = FALSE
+    for (colnam in names(df_pow)[startsWith(names(df_pow), 'alpha_local_')]) {
+        if ('possa_p' %in% class(df_pow[[colnam]])) {
+            df_p_names = c(df_p_names, colnam)
+            df_p_nams_all = c(df_p_nams_all, colnam)
+        } else if ('possa_p_nonstopper' %in% class(df_pow[[colnam]])) {
+            df_p_nams_all = c(df_p_nams_all, colnam)
+        }
+    }
+    df_p_names = gsub('alpha_local_', '', df_p_names)
+    df_p_nams_all = gsub('alpha_local_', '', df_p_nams_all)
+    for (futcol in names(df_pow)[startsWith(names(df_pow), 'futil_local_')]) {
+        if ('possa_futility' %in%
+            class(df_pow[[futcol]])) {
+            futilities = TRUE
+        }
+    }
+    if (length(df_p_names) > 1) {
+        multiple_ps = TRUE
+    } else {
+        multiple_ps = FALSE
+    }
+    cat(
+        'N(average-total) = \033[0;4m',
+        ro(df_pow$n_avg_prop_0[df_nrow], 1, leading_zero = TRUE),
+        '\033[0m (if H0 true) or \033[0;4m',
+        ro(df_pow$n_avg_prop_1[df_nrow], 1, leading_zero = TRUE),
+        '\033[0m (if H1 true)',
+        sep = '',
+        fill = TRUE
+    )
+    fut_text = ''
+    for (p_nam in df_p_nams_all) {
+        if (p_nam %in% df_p_names) {
+            nonstp = ''
+            l_a_descr = '\nLocal alphas: '
+        } else {
+            nonstp = '\033[0;40;3mnon-stopper: \033[0;40;0m'
+            l_a_descr = '\nLocal alphas (secondary): '
+        }
+        p_a_locals = ro(df_pow[[paste0('alpha_local_', p_nam)]][looks], round_to)
+        p_a_locals = ifelse(p_a_locals == '0',
+                            'none',
+                            paste0('\033[0;1m', p_a_locals, '\033[0m'))
+        if (mlook > 1) {
+            sign_ratios = paste0(
+                '\n\033[0;3mLikelihoods of significance if H0 true: ',
+                paste(paste0(
+                    '(',
+                    looks,
+                    ') ',
+                    ro(df_pow[[paste0('ratio_sign_', p_nam, '_h0')]][looks], round_to),
+                    collapse = '; '
+                )),
+                '\nLikelihoods of significance if H1 true: ',
+                paste(paste0(
+                    '(',
+                    looks,
+                    ') ',
+                    ro(df_pow[[paste0('ratio_sign_', p_nam, '_h1')]][looks], round_to),
+                    collapse = '; '
+                )),
+                '\033[0m'
+            )
+        } else {
+            sign_ratios = ''
+        }
+        toprint = paste0(
+            '(',
+            nonstp,
+            '\033[0;40m',
+            p_nam,
+            '\033[0m) Type I error: \033[1;31m',
+            ro(df_pow[[paste0('ratio_sign_', p_nam, '_h0')]][df_nrow], round_to),
+            '\033[0m; Power: \033[0;32m',
+            ro(df_pow[[paste0('ratio_sign_', p_nam, '_h1')]][df_nrow], round_to),
+            '\033[0m',
+            l_a_descr,
+            paste(paste0(
+                '(',
+                looks,
+                ') ',
+                p_a_locals,
+                collapse = '; '
+            )),
+            sign_ratios
+        )
+        cat(toprint, fill = TRUE)
+        if (futilities) {
+            if (all(df_pow[[paste0('futil_local_', p_nam)]][looks[-mlook]] == 1)) {
+                cat('Futility bounds: none', fill = TRUE)
+            } else {
+                p_fa_locals = ro(df_pow[[paste0('futil_local_', p_nam)]][looks[-mlook]], round_to)
+                p_fa_locals = ifelse(
+                    p_fa_locals == '0',
+                    'none',
+                    paste0('\033[0;1m',
+                           p_fa_locals,
+                           '\033[0m')
+                )
+                cat(
+                    paste0(
+                        'Futility bounds: ',
+                        paste(
+                            paste0('(',
+                                   looks[-mlook],
+                                   ') ',
+                                   p_fa_locals),
+                            collapse = '; '
+                        ),
+                        '\n\033[0;3mLikelihoods of exceeding futility alpha if H0 true: ',
+                        paste(paste0(
+                            '(',
+                            looks[-mlook],
+                            ') ',
+                            ro(df_pow[[paste0('ratio_futil_', p_nam, '_h0')]][looks[-mlook]], round_to),
+                            collapse = '; '
+                        )),
+                        '\nLikelihoods of exceeding futility alpha if H1 true: ',
+                        paste(paste0(
+                            '(',
+                            looks[-mlook],
+                            ') ',
+                            ro(df_pow[[paste0('ratio_futil_', p_nam, '_h1')]][looks[-mlook]], round_to),
+                            collapse = '; '
+                        )),
+                        '\033[0m'
+                    ),
+                    fill = TRUE
+                )
+            }
+        }
+    }
+    if (multiple_ps) {
+        toprint = paste0(
+            '\033[0;4mGlobal\033[0m ("combined significance") type I error: ',
+            ro(df_pow$ratio_combined_sign_h0[df_nrow],
+               # same as global_type1
+               round_to),
+            ' (included: ',
+            paste(df_p_names, collapse = ', '),
+            '; power for reaching the "combined significance": ',
+            ro(df_pow$ratio_combined_sign_h1[df_nrow],
+               # same as global_power
+               round_to),
+            ')'
+        )
+        if (mlook > 1) {
+            toprint = paste0(
+                toprint,
+                '\n\033[0;3mLikelihoods of stopping for (combined) \033[1msignificance\033[0;3m if H0 true: ',
+                paste(paste0(
+                    '(',
+                    looks,
+                    ') ',
+                    ro(df_pow$ratio_combined_sign_h0[looks],
+                       round_to),
+                    collapse = '; '
+                )),
+                '\nLikelihoods of stopping for (combined) \033[1msignificance\033[0;3m if H1 true: ',
+                paste(paste0(
+                    '(',
+                    looks,
+                    ') ',
+                    ro(df_pow$ratio_combined_sign_h1[looks],
+                       round_to),
+                    collapse = '; '
+                )),
+                '\033[0m'
+            )
+            if (futilities) {
+                toprint = paste0(
+                    toprint,
+                    '\n\033[0;3mLikelihoods of stopping for (combined) \033[1mfutility\033[0;3m if H0 true: ',
+                    paste(paste0(
+                        '(',
+                        looks[-mlook],
+                        ') ',
+                        ro(df_pow$ratio_combined_fut_h0[looks[-mlook]],
+                           round_to),
+                        collapse = '; '
+                    )),
+                    '\nLikelihoods of stopping for (combined) \033[1mfutility\033[0;3m if H1 true: ',
+                    paste(paste0(
+                        '(',
+                        looks[-mlook],
+                        ') ',
+                        ro(df_pow$ratio_combined_fut_h1[looks[-mlook]],
+                           round_to),
+                        collapse = '; '
+                    )),
+                    '\033[0m'
+                )
+            }
+        }
+        cat(toprint, fill = TRUE)
+    }
+}
---
+++ R/print.possa_sim_df.R
@@ -0,0 +1,151 @@
+#'@title Print sim results
+#'
+#'@description Prints information about the simulated p values created by the
+#'  \code{\link[POSSA:sim]{POSSA::sim}} function. This is an extension (method)
+#'  of the base R \code{\link[print]} function, so it can be called simply as
+#'  \code{print()}.
+#'@param df_sim The \code{\link{data.frame}} returned by the
+#'  \code{\link[POSSA:sim]{POSSA::sim}} function.
+#'@param descr_cols When given as a character element or vector, specifies the
+#'  factors for which descriptive data should be shown (by group, if
+#'  applicable). By default \code{NULL}, it identifies (similar as
+#'  \code{group_by}) factors, if any, given to the \code{sim} function (via
+#'  \code{fun_obs}) that produced the given \code{df_sim} data.
+#'@param descr_func Function used for printing descriptives (see
+#'  \code{descr_cols}). By default, it uses the \code{\link{summary}}
+#'  (\code{\link{base}}) function.
+#'@param group_by When given as a character element or vector, specifies the
+#'  factors by which to group the descriptives: the \code{df_sim} data will be
+#'  divided into parts by these factors and these parts will be analyzed
+#'  separately, with descriptives printed per each part. By default
+#'  (\code{NULL}), it identifies factors, if any, given to the \code{sim}
+#'  function (via \code{fun_obs}) that created the given \code{df_sim} data.
+#'
+#'@seealso \code{\link{sim}}
+#'
+#' @export
+print.possa_sim_df = function(df_sim,
+                              group_by = NULL,
+                              descr_cols = TRUE,
+                              descr_func = summary,
+                              ...) {
+    cat(
+        format.possa_sim_df(
+            df_sim = df_sim,
+            group_by = group_by,
+            descr_cols = descr_cols,
+            descr_func = descr_func,
+            ...
+        ),
+        fill = TRUE
+    )
+}
+
+format.possa_sim_df = function(df_sim,
+                               group_by = NULL,
+                               descr_cols = TRUE,
+                               descr_func = summary,
+                               ...) {
+    cat('\033[0;34mPOSSA sim() results (p values)\033[0m', fill = TRUE)
+    df_sim = data.table::copy(df_sim)
+    validate_args(match.call(),
+                  list(
+                      val_arg(df_sim, c('df')),
+                      val_arg(group_by, c('null', 'char')),
+                      val_arg(descr_cols, c('bool', 'char')),
+                      val_arg(descr_func, c('function'), 1)
+                  ))
+    cat('\033[0;37mSample:\033[0m', fill = TRUE)
+
+    setDT(df_sim)
+    print(utils::head(df_sim, min(max(df_sim$.look), 6L)))
+    setkey(df_sim, .look)
+
+    n_cols = c()
+    p_names_all = c()
+    fac_cols = c()
+    for (c_nam in colnames(df_sim)) {
+        col = df_sim[[c_nam]]
+        if ('possa_n' %in% class(col)) {
+            n_cols = c(n_cols, c_nam)
+        } else if ('possa_fac' %in% class(col)) {
+            fac_cols = c(fac_cols, c_nam)
+        } else if (startsWith(c_nam, 'p_') &&
+                   endsWith(c_nam, '_h0')) {
+            p_nam_h1 = paste0(substr(c_nam, 1, nchar(c_nam) - 3), '_h1')
+            if (p_nam_h1 %in% colnames(df_sim)) {
+                p_names_all = c(p_names_all, c_nam)
+                p_names_all = c(p_names_all, p_nam_h1)
+            }
+        }
+    }
+    if (is.null(group_by)) {
+        group_by = fac_cols
+    }
+    if (length(group_by) > 0) {
+        df_sim[, ._possa_fact_combs := do.call(paste, c(.SD, sep = '; ')), .SDcols = group_by]
+        possafacts = unique(df_sim$._possa_fact_combs)
+        setindex(df_sim, ._possa_fact_combs)
+    } else {
+        possafacts = NA
+    }
+    if (descr_cols[1] == TRUE) {
+        descr_cols = names(df_sim)[!names(df_sim) %in%
+                                       c(
+                                           '.iter',
+                                           '.look',
+                                           '.n_total',
+                                           '._possa_fact_combs',
+                                           p_names_all,
+                                           n_cols,
+                                           fac_cols
+                                       )]
+        if (!length(descr_cols) > 0) {
+            descr_cols = FALSE
+        }
+    }
+    if (descr_cols[1] != FALSE) {
+        cat('\033[0;37mDescriptives:\033[0m', fill = TRUE)
+        for (possa_fact in possafacts) {
+            # when none: possa_fact = NA
+            if (is.na(possafacts[1])) {
+                pvals_df = df_sim
+            } else {
+                # print descriptives of all included
+                if (possa_fact == possafacts[1]) {
+                    cat('\033[0;40mTOTAL (all groups)\033[0m',
+                        fill = TRUE)
+                    for (desc_col in descr_cols) {
+                        cat(
+                            '\033[0;3m',
+                            desc_col,
+                            ':\033[0m ',
+                            sep = '',
+                            fill = TRUE
+                        )
+                        print(descr_func(df_sim[[desc_col]]))
+                    }
+                }
+                # if applicable, take only given factor combination & print its "group" name
+                pvals_df = df_sim[._possa_fact_combs == possa_fact]
+                cat(
+                    '\nGROUP (',
+                    paste(group_by, collapse = '; '),
+                    '): \033[0;40m',
+                    possa_fact,
+                    '\033[0m',
+                    fill = TRUE,
+                    sep = ''
+                )
+            }
+            for (desc_col in descr_cols) {
+                cat('\033[0;3m',
+                    desc_col,
+                    ':\033[0m ',
+                    sep = '',
+                    fill = TRUE)
+                print(descr_func(pvals_df[[desc_col]]))
+            }
+        }
+    }
+}
